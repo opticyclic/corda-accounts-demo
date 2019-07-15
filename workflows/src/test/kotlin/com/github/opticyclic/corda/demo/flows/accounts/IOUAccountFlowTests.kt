6a033e0b9cd418176082476d4d18a5aec656d4e2
opticyclic <opticyclic@gmail.com>
2019-07-15 17:01:24 -0400

    Rename megaCorp and miniCorp to banks and agents in preparation of using accounts

--- workflows/src/test/kotlin/com/github/opticyclic/corda/demo/flows/accounts/IOUAccountFlowTests.kt
+++ workflows/src/test/kotlin/com/github/opticyclic/corda/demo/flows/accounts/IOUAccountFlowTests.kt
@@ -17,8 +17,8 @@ import kotlin.test.assertEquals
 
 class IOUAccountFlowTests {
     private lateinit var network: MockNetwork
-    private lateinit var megaCorp: StartedMockNode
-    private lateinit var miniCorp: StartedMockNode
+    private lateinit var banks: StartedMockNode
+    private lateinit var agents: StartedMockNode
 
     @BeforeClass
     fun setup() {
@@ -28,10 +28,10 @@ class IOUAccountFlowTests {
                         TestCordapp.findCordapp("com.github.opticyclic.corda.demo.accounts.contracts"),
                         TestCordapp.findCordapp("com.github.opticyclic.corda.demo.flows.accounts")
                 )))
-        megaCorp = network.createPartyNode()
-        miniCorp = network.createPartyNode()
+        banks = network.createPartyNode()
+        agents = network.createPartyNode()
         // For real nodes this happens automatically, but we have to manually register the flow for tests.
-        listOf(megaCorp, miniCorp).forEach { it.registerInitiatedFlow(IOUAccountResponder::class.java) }
+        listOf(banks, agents).forEach { it.registerInitiatedFlow(IOUAccountResponder::class.java) }
         network.runNetwork()
     }
 
@@ -43,43 +43,43 @@ class IOUAccountFlowTests {
     @Test
     fun `recorded transaction has no inputs and a single output, the input IOU`() {
         val iouValue = 1
-        val flow = IOUAccountFlow(iouValue, miniCorp.info.singleIdentity())
-        val future = megaCorp.startFlow(flow)
+        val flow = IOUAccountFlow(iouValue, agents.info.singleIdentity())
+        val future = banks.startFlow(flow)
         network.runNetwork()
         val signedTx = future.getOrThrow()
 
         // We check the recorded transaction in both vaults.
-        for (node in listOf(megaCorp, miniCorp)) {
+        for (node in listOf(banks, agents)) {
             val recordedTx = node.services.validatedTransactions.getTransaction(signedTx.id)
             val txOutputs = recordedTx!!.tx.outputs
             assert(txOutputs.size == 1)
 
             val recordedState = txOutputs[0].data as IOUAccountState
             assertEquals(recordedState.value, iouValue)
-            assertEquals(recordedState.lender, megaCorp.info.singleIdentity())
-            assertEquals(recordedState.borrower, miniCorp.info.singleIdentity())
+            assertEquals(recordedState.lender, banks.info.singleIdentity())
+            assertEquals(recordedState.borrower, agents.info.singleIdentity())
         }
     }
 
     @Test
     fun `flow records the correct IOU in both parties' vaults`() {
         val iouValue = 5
-        val flow = IOUAccountFlow(iouValue, miniCorp.info.singleIdentity())
-        val future = megaCorp.startFlow(flow)
+        val flow = IOUAccountFlow(iouValue, agents.info.singleIdentity())
+        val future = banks.startFlow(flow)
         network.runNetwork()
         val signedTx = future.getOrThrow()
 
         val output = signedTx.tx.outputs.single().data as IOUAccountState
         // We check the recorded IOU in both vaults.
-        for (node in listOf(megaCorp, miniCorp)) {
+        for (node in listOf(banks, agents)) {
             node.transaction {
                 val linearStateCriteria = QueryCriteria.LinearStateQueryCriteria(linearId = listOf(output.linearId))
                 val ious = node.services.vaultService.queryBy<IOUAccountState>(linearStateCriteria).states
                 assertEquals(1, ious.size)
                 val recordedState = ious.single().state.data
                 assertEquals(recordedState.value, iouValue)
-                assertEquals(recordedState.lender, megaCorp.info.singleIdentity())
-                assertEquals(recordedState.borrower, miniCorp.info.singleIdentity())
+                assertEquals(recordedState.lender, banks.info.singleIdentity())
+                assertEquals(recordedState.borrower, agents.info.singleIdentity())
             }
         }
     }
